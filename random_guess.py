dcac8c0621f077dbd85c69b37b60880b3024359e
Eduardo Felix Frois <103344449+dudsbtw@users.noreply.github.com>
2022-07-08 21:19:15 -0300

    Update random_guess.py

--- random_guess.py
+++ random_guess.py
@@ -1,7 +1,7 @@
 import random
 random_number = random.randint(1,10)
 hit = False
-while hit == False
+while hit == False:
     guess = int(input('Guess a number from 1 to 10 '))
     if guess > random_number:
         print('Your guess was higher than the random number.')
