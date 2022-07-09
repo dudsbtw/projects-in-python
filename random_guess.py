b171bd284a201dddf6ed94cd86d177191b3b435e
Eduardo Felix Frois <103344449+dudsbtw@users.noreply.github.com>
2022-07-08 21:14:34 -0300

    Update random_guess.py

--- random_guess.py
+++ random_guess.py
@@ -1,9 +1,12 @@
 import random
 random_number = random.randint(1,10)
-guess = int(input('Guess a number from 1 to 10 '))
-if guess > random_number:
-    print('Your guess was higher than the random number.')
-elif guess < random_number:
-    print('Your guess was lower than the random number.')
-elif guess == random_number:
-    print('You are right!')
+hit = False
+while hit == False
+    guess = int(input('Guess a number from 1 to 10 '))
+    if guess > random_number:
+        print('Your guess was higher than the random number.')
+    elif guess < random_number:
+        print('Your guess was lower than the random number.')
+    elif guess == random_number:
+        hit = True
+        print('You are right!')
